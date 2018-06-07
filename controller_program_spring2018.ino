f95bf03255ec84d3be7600e04cc0c24697838c16
TomatoBobcat <2480292220@qq.com>
2018-06-06 23:43:42 -0700

    <Spring18 final version> complete comments

--- controller_program_spring2018.ino
+++ controller_program_spring2018.ino
@@ -1,62 +1,96 @@
+/*
+ * controller_program_spring2018.ino
+ *   Created: March 2018
+ *   Updated: June 6, 2018
+ *   Author: Steven, Asis
+ *
+ * >> Implemented:
+ *   1.nrf	2.LCD screen
+ *
+ *   <Final version for Spring2018>
+ *     The script implements the controller part of the radio-controlled
+ *    proabot.
+ */
+
+
 #include "Arduino.h"
 #include <SPI.h>
 #include <RF24.h>
-#include "LiquidCrystal_I2C_v2.h"
+#include "LiquidCrystal_I2C.h"
 
+#define HIGH_CURRENT 7    // The same encoding is used in "motor.h"
 
 
 // Hardware configuration: Set up nRF24L01 radio on SPI bus (pins 10, 11, 12, 13) plus pins 7 & 8
-RF24 radio(9, 53);
-byte addresses[][6] = {"1Node", "2Node"};
-double data_to_send[2]; // [0] setpoint_1; [1] setpoint_2
-double data_receive[4]; // [0] rudder_position_1; [1] rudder_position_2; [2] rudder_current_1; [3] rudder_current_2
+RF24 radio(9, 53);  //CE, CSN pins
+byte addresses[][6] = {"00001", "00002"};  //names of the two communications(2 directions)
+
+//[0] setpoint_1; [1] setpoint_2
+double data_to_send[2];
+//[0] rudder_position_1; [1] rudder_position_2; [2] rudder_current_1; [3] rudder_current_2
+double data_receive[4];
 
 LiquidCrystal_I2C lcd(0x3F,16,2);
+
+void radioSetup_controller()
+{
+	// Initiate the radio object
+	radio.begin();
+	// Set the transmit power to lowest available to prevent power supply related issues
+	radio.setPALevel(RF24_PA_MIN);
+	// Set the speed of the transmission to the quickest available
+	radio.setDataRate(RF24_2MBPS);
+	// Use a channel unlikely to be used by Wifi, Microwave ovens etc
+	radio.setChannel(124);
+	// Open a writing and reading pipe on each radio, with opposite addresses
+	radio.openWritingPipe(addresses[1]);
+	radio.openReadingPipe(1, addresses[0]);
+}
 
 
 void setup() {
   Serial.begin(9600);
 
-  // Initiate the radio object
-  radio.begin();
-  // Set the transmit power to lowest available to prevent power supply related issues
-  radio.setPALevel(RF24_PA_MIN);
-  // Set the speed of the transmission to the quickest available
-  radio.setDataRate(RF24_2MBPS);
-  // Use a channel unlikely to be used by Wifi, Microwave ovens etc
-  radio.setChannel(124);
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1, addresses[0]);
+  radioSetup_controller();
+  /* legacy code: These two pins were used to give potentiometer 5V
+  pinMode(31,OUTPUT);
+  pinMode(33,OUTPUT);
+  digitalWrite(31,HIGH);
+  digitalWrite(33,LOW);
+  */
+
+
 
   lcd.begin();
   lcd.backlight();
-  pinMode(A1, INPUT);// this will read the pot. to control rudder possition.
+  pinMode(A1, INPUT);// A1 will read the pot. to control rudder position.
+  pinMode(A2, INPUT);
 }
 
 void show_info(){
 	lcd.setCursor(0, 0);
-	lcd.print(data_receive[0]);
-	lcd.setCursor(7,0);
 	lcd.print(data_receive[1]);
-	if (data_receive[2]==7){
-		lcd.setCursor(7,0);
+	lcd.setCursor(7,0);
+	lcd.print(data_receive[0]);
+	if (data_receive[3]==HIGH_CURRENT){
+		lcd.setCursor(6,0);
 		lcd.print("H");
 	}
-	if(data_receive[3]==7){
+	if(data_receive[2]==HIGH_CURRENT){
 		lcd.setCursor(15,0);
 		lcd.print("H");
 	}
+
 	lcd.setCursor(0,1);
-	lcd.print(data_to_send[0]);
-	lcd.setCursor(7,1);
 	lcd.print(data_to_send[1]);
+	lcd.setCursor(7,1);
+	lcd.print(data_to_send[0]);
 
 }
 
 void update_setpoint(){
 	data_to_send[0] = analogRead(A1); //r1_setpoint
-	data_to_send[1] = 50;
+	data_to_send[1] = analogRead(A2); //r2_setpoint
 }
 
 void radio_write(){
@@ -96,14 +130,19 @@ void radio_read(){
 void monitor_print(){
 	// Show user what we sent and what we got back
 	Serial.print("Sent setpoint: ");
-	Serial.println(data_to_send[0]);
+	Serial.print(data_to_send[0]);
+	Serial.print("\t");
+	Serial.println(data_to_send[1]);
+
 	Serial.print("Received:\n");
 	Serial.print("\tr1 position: ");
 	Serial.println(data_receive[0]);
 	Serial.print("\tr2 position: ");
 	Serial.println(data_receive[1]);
-	if (data_receive[3]==7)
+	if (data_receive[2]==7)
 		Serial.println("\t r1 stopped due to HIGH_CURRENT_ERROR");
+	if (data_receive[3]==7)
+		Serial.println("\t r2 stopped due to HIGH_CURRENT_ERROR");
 }
 
 void loop() {
@@ -115,5 +154,4 @@ void loop() {
 	radio_read();
 
 	monitor_print();
-
 }
